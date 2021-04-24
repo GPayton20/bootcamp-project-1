b499c3fd0b5ad85550314d5657d4eaa0f2f15823
Geoffrey <geoffrpayton@gmail.com>
2021-04-24 13:39:51 -0400

    completed blog form functionality

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
 const app = {};
 
+app.init = function() {
+  // Add event listener to blog section form
+  const contactForm = document.querySelector('.contactForm');
+  contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    app.validateFormInput(app.printComment);
+  })
+}
+
 app.validateFormInput = function(callback) {
+  // Get values from inputs
   const userName = document.getElementById('name').value;
   const userEmail = document.getElementById('email').value;
   const userMessage = document.querySelector('textarea').value;
 
+  // If user has input valid values
   if (userName && userEmail && userMessage) {
+    // Pass name and message to callback function
     callback(userName, userMessage);
+    // Clear inputs
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.querySelector('textarea').value = '';
   }
+
 }
 
 app.printComment = function(name, comment) {
   const newComment = document.createElement('article');
   newComment.classList.add('commentPost');
-  const date = new Date();
+  // Get current date in human-readable format
+  const date = new Date().toDateString();
   
+  // Build boilerplate HTML for new comment
   newComment.innerHTML = `
-  <img src="./assets/blog-image-3.jpg" alt="Barb profile image">
+  <img src="http://placekitten.com/90/90" alt="User profile image">
   <div class="textContainer">
-    <span class="byline">${date.toDateString()} by ${name}</span>
+    <span class="byline">${date} by ${name}</span>
     <p></p>
   </div>
   `;
 
+  // Add comment text in secure fashion
   newComment.querySelector('p').innerText = comment;
-
-  console.log(newComment);
-
-  // document.querySelector('.blogSection').appendChild(newComment);
 
   const blogSection = document.querySelector('.blogSection');
   const form = document.querySelector('form');
 
+  // Add new comment to section, after last comment but before form
   blogSection.insertBefore(newComment, form);
 
 }
+
+
+app.init();
