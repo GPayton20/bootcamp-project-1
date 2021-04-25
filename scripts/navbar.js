f73bac95170bda5320e8f33cc165923999aeb085
Geoffrey <geoffrpayton@gmail.com>
2021-04-25 14:23:46 -0400

    menu closes when About link is clicked

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,16 @@
+// Get elements from DOM
 const menuButton = document.querySelector('.hamburger');
 const menuBar = document.querySelector('.navBar ul');
+const aboutLink = document.getElementById('aboutLink');
 
-menuButton.addEventListener('click', () => {
+// Open/close side nav menu, change hamburger icon styling
+function changeMenu() {
   menuButton.classList.toggle('arrow');
   menuBar.classList.toggle('open');
-})
+}
+
+// Change nav on hamburger/arrow click
+menuButton.addEventListener('click', changeMenu);
+
+// Close menu when 'About' link is clicked
+aboutLink.addEventListener('click', changeMenu);
